f7416fc7b4573214fc55e74b9da1038de710d6fb
d.shaikhullin <d.shaikhullin@mail.business.ru>
2024-05-17 12:34:47 +0500

    removed date field

--- lib/domain/entity/task.dart
+++ lib/domain/entity/task.dart
@@ -1,26 +1,23 @@
 import 'package:equatable/equatable.dart';
 
 class Task extends Equatable {
-  final int id;
+  final String id;
   final String title;
   final String description;
-  final DateTime dueDate;
   final bool isCompleted;
 
   const Task({
     required this.id,
     required this.title,
     required this.description,
-    required this.dueDate,
-    required this.isCompleted,
+    this.isCompleted = false,
   });
 
   factory Task.fromMap(Map<String, dynamic> json) {
     return Task(
-      id: json['id'] as int,
+      id: json['id'] as String,
       title: json['title'] as String,
       description: json['description'] as String,
-      dueDate: DateTime.parse(json['dueDate'] as String),
       isCompleted: json['isCompleted'] as bool,
     );
   }
@@ -30,12 +27,15 @@ class Task extends Equatable {
       'id': id,
       'title': title,
       'description': description,
-      'dueDate': dueDate.toIso8601String(),
       'isCompleted': isCompleted,
     };
   }
       
     @override
-    // TODO: implement props
-    List<Object?> get props => throw UnimplementedError();
+    List<Object?> get props => [
+      id,
+      title,
+      description,
+      isCompleted,
+    ];
 }
