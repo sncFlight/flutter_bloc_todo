a261a89014401db49feb161c9810156b643f50b3
d.shaikhullin <d.shaikhullin@mail.business.ru>
2024-05-17 12:45:55 +0500

    set portrait orientation && and initializaing sharedPreferences

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,20 @@
-runApp() {
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'package:bloc_to_do/application.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Initialize shared preferences from storage
+  final SharedPreferences sharedPref = await SharedPreferences.getInstance();
   
+  // Set preferred device orientation to portrait
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
+
+  runApp(Application(sharedPref: sharedPref));
 }
